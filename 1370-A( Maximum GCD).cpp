ca13859470a2018576275f058bf85cf11da19669
Ishrat29 <52996520+Ishrat29@users.noreply.github.com>
2020-08-05 18:01:46 +0600

    1370-A

---
+++ 1370-A( Maximum GCD).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+   long long n,x;
+    cin>>n;
+     x=(n/2);
+    cout<<x<<endl;
+   }
+   return 0;
+}
