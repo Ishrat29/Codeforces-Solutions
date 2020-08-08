7f0a29dd45229fe97c687f89ddace84f4df7f714
Ishrat29 <52996520+Ishrat29@users.noreply.github.com>
2020-08-09 01:07:27 +0600

    Solution

---
+++ 1372-A( Omkar and Completion).cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+
+
+    }
+     return 0;
+}
+
