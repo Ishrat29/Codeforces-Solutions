f51de6c7ba39bb55a373fc32449bbb668ea52677
Ishrat29 <52996520+Ishrat29@users.noreply.github.com>
2020-08-09 00:56:15 +0600

    Solution

---
+++ 1393-A(Rainbow Dash, Fluttershy and Chess Coloring).cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,x;
+        cin>>n;
+        x=(n/2)+1;
+        cout<<x<<endl;
+
+    }
+return 0;
+}
