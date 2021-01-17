15c3a408064c151f2b8fa57f6321967cb8dfb2ed
Ishrat29 <52996520+Ishrat29@users.noreply.github.com>
2021-01-18 01:49:54 +0600

    Solution

---
+++ 1296-B(Food Buying).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define f(i, a) for (int i=0; i<(a); i++)
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       ll s,n,k=0;
+       cin>>s;
+       cout<<s+(s-1)/9<<endl;
+    }
+   return 0;
+}
