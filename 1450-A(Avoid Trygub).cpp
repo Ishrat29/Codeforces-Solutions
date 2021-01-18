b5fbe9ea60637a6af83916f8c2276fab22c6a96f
Ishrat29 <52996520+Ishrat29@users.noreply.github.com>
2021-01-19 01:38:54 +0600

    Solution

---
+++ 1450-A(Avoid Trygub).cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define f(i, a) for (int i=0; i<(a); i++)
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,d,x=0,i,m=0;
+        cin>>n;
+        string s,s1;
+        cin>>s;
+        sort(s.begin(),s.end());
+        cout<<s<<endl;
+
+    }
+   return 0;
+}
