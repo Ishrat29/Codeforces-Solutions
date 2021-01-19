93cc86180d81d30a4abd38fcb777417e615cb2fe
Ishrat29 <52996520+Ishrat29@users.noreply.github.com>
2021-01-19 12:14:11 +0600

    Solution

---
+++ 1353-A(Most Unstable Array).cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define f(i, a) for (int i=0; i<(a); i++)
+using namespace std;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,d,x=0,i,m=0;
+        cin>>n>>d;
+        if(n==1) cout<<0<<endl;
+        else if(n==2) cout<<d<<endl;
+        else cout<<d*2<<endl;
+    }
+   return 0;
+}
